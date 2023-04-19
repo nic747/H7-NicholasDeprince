6e1358508b4e08179fd78b691f88e5d35bc5f8e8
Nicholas Deprince <nicholas.deprince@student.ppsjo.be>
2023-04-19 11:49:22 +0200

    test nicholas1 changes

--- H7-NicholasDeprince/Form1.Designer.cs
+++ H7-NicholasDeprince/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.DeprinceNicholas = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // DeprinceNicholas
@@ -41,11 +42,21 @@
             this.DeprinceNicholas.UseVisualStyleBackColor = true;
             this.DeprinceNicholas.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "test nicholas";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.DeprinceNicholas);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Button DeprinceNicholas;
+        private System.Windows.Forms.Button button1;
     }
 }
 
