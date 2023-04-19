dd3368a056dbd25945287258d5d91e917ac44d73
Nicholas Deprince <nicholas.deprince@student.ppsjo.be>
2023-04-19 11:59:11 +0200

    TESTJE

--- H7-NicholasDeprince/Form1.Designer.cs
+++ H7-NicholasDeprince/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.DeprinceNicholas = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.test3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // DeprinceNicholas
@@ -61,17 +62,29 @@
             this.test3.Text = "Castle test";
             this.test3.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(335, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "jqhsfqsDF";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.test3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.DeprinceNicholas);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,6 +93,7 @@
         private System.Windows.Forms.Button DeprinceNicholas;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button test3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- H7-NicholasDeprince/Form1.cs
+++ H7-NicholasDeprince/Form1.cs
@@ -21,5 +21,10 @@ namespace H7_NicholasDeprince
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
